c49effe9730ff528c53604bdb99654081b99b242
Michele <mpdo2017@gmail.com>
2019-07-12 15:31:06 -0400

    Initial commit

---
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/CryptoUtils.java
@@ -0,0 +1,20 @@
+package encryptdecrypt;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+
+/*utility class that encrypts/decrypts a file */
+
+/**
+ * @author Michele Lanning
+ * @version 2019-07-12
+ */
+public class CryptoUtils {
+}
---
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/test/CryptoUtilsTest.java
@@ -0,0 +1,8 @@
+package encryptdecrypt.test;
+
+/**
+ * @author Wes Lanning
+ * @version 2019-07-12
+ */
+public class CryptoUtilsTest {
+}
